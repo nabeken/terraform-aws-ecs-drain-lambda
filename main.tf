4898bf4ed9db4a289fd935adb9eb75fbfbf53a9f
Tanabe Ken-ichi <nabeken@tknetworks.org>
2023-03-14 18:35:28 +0900

    fix the IAM policy

--- main.tf
+++ main.tf
@@ -40,8 +40,7 @@ data "aws_iam_policy_document" "main" {
     ]
 
     resources = [
-      "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/${var.prefix}-ecs-drain",
-      "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/${var.prefix}-ecs-drain/*",
+      "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/${var.prefix}-ecs-drain:*",
     ]
   }
 }
