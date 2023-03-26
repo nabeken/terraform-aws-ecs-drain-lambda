84cba1bb2b5c1f3fb973f78a7a9c2572551b6a24
TANABE Ken-ichi <nabeken@tknetworks.org>
2023-03-26 15:22:40 +0900

    make the linter happy

--- example/variables.tf
+++ example/variables.tf
@@ -1,6 +1,13 @@
-variable "region" {}
-variable "prefix" {}
+variable "region" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
 variable "drain_asg_names" {
-  type    = list(any)
-  default = []
+  description = "Name of Auto Scaling Group that the lambda function reacts. If you don't specify this, the lambda function will react to all of Auto Scaling Group in the account. You can use the comparison operators available in EventBridge."
+  type        = list(any)
+  default     = []
 }
